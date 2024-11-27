9bc0d3b39ebf5427d419371e55fe80537c5a9a27
Muraups <muriloaugusto.p9@edu.unifil.br>
2024-11-27 00:46:28 -0300

    Ajustes finais

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const albumRoutes = require('./routes/albumRoutes');
 const trackRoutes = require('./routes/trackRoutes');
 const artistRoutes = require('./routes/artistRoutes'); // Novas rotas de Artist
 const genreRoutes = require('./routes/genreRoutes'); // Novas rotas de Genre
+const { Track, Album, Artist } = require('./models');
 const { sequelize } = require('./models/index'); // Importa o Sequelize e associações
 
 // Cria o app e configura middlewares
@@ -21,7 +22,6 @@ app.use('/api/tracks', trackRoutes);
 app.use('/api/artists', artistRoutes); 
 app.use('/api/genres', genreRoutes); 
 
-// Rotas para servir as páginas HTML
 app.get("/html/artists.html", (req, res) => {
     res.sendFile(path.join(__dirname, "public/html/artists.html"));
   });
@@ -36,25 +36,51 @@ app.get("/html/artists.html", (req, res) => {
   });
 
   app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html')); // Envia o arquivo HTML principal
+    res.sendFile(path.join(__dirname, 'index.html')); 
   });
   
-  app.get('/artists', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/html/artists.html')); // Envia o arquivo artists.html
+  app.get('/api/tracks', async (req, res) => {
+    try {
+      const tracks = await Track.findAll({
+        include: [
+          {
+            model: Album,
+            include: [Artist], // Inclui o Artista dentro do Álbum
+          },
+        ],
+      });
+  
+      console.log(tracks); // Verifique os dados retornados no console
+      res.json(tracks); // Envia as faixas com a duração e as associações
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Erro ao carregar faixas' });
+    }
   });
+  
+  app.get('/api/albums', async (req, res) => {
+    try {
+      const albums = await Album.findAll({
+        include: [
+          {
+            model: Artist, // Certifique-se de que é o modelo Artist e não Album
+            as: 'Artist', // A associação que você fez com Artist (se necessário)
+          },
+        ],
+      });
+      res.json(albums); // Envia os álbuns com os dados do artista
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Erro ao carregar álbuns' });
+    }
+  });
+  
+  
+  
 
-// Rota principal
-/*app.get('/', (req, res) => {
-  res.json({
-    message: 'Bem-vindo à API de Catálogo de Discos!',
-    endpoints: {
-      albums: '/api/albums',
-      tracks: '/api/tracks',
-      artists: '/api/artists',
-      genres: '/api/genres',
-    },
+  app.get('/artists', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/html/artists.html')); 
   });
-});*/
 
 sequelize
   .sync({ alter: true })
